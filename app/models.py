c083b51a9220762bd4afdf3b37f71e94a1f84331
delawer33 <shukry.aliev@mail.ru>
2024-11-21 13:54:55 +0300

    created TodoItem model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from myauth.models import User
+
+
+class TodoItem(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=500)
+    done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
